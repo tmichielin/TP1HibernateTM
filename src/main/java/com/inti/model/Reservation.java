5fe5a85b70ad5a9796f1f3f7023e85ef19462f86
zwileo <leo.zwilling@orange.fr>
2023-03-07 14:18:57 +0100

    Relation Aeroport avec Vol

--- src/main/java/com/inti/model/Reservation.java
+++ src/main/java/com/inti/model/Reservation.java
@@ -16,6 +16,8 @@ public class Reservation {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idReservation;
+	
     private int numero;
     private LocalDate date;
     
@@ -29,10 +31,6 @@ public class Reservation {
     public Reservation() {
     }
 
-
-
-    
-    
     
     public void Annuler() {
         // TODO implement here
@@ -43,4 +41,63 @@ public class Reservation {
         // TODO implement here
     }
 
+
+	public Reservation(int numero, LocalDate date, Client client) {
+		super();
+		this.numero = numero;
+		this.date = date;
+		this.client = client;
+	}
+
+
+	public int getIdReservation() {
+		return idReservation;
+	}
+
+
+	public void setIdReservation(int idReservation) {
+		this.idReservation = idReservation;
+	}
+
+
+	public int getNumero() {
+		return numero;
+	}
+
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+
+	public Client getClient() {
+		return client;
+	}
+
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Reservation [idReservation=" + idReservation + ", numero=" + numero + ", date=" + date + ", client="
+				+ client + "]";
+	}
+    
+    
+    
+    
+    
 }
