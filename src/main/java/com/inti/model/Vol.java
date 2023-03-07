0c9ce7cc2ac3711589bd634dc06955c231ad4e28
Thaou15 <thaoura.mhoma@laposte.net>
2023-03-07 14:51:39 +0100

    Vol

--- src/main/java/com/inti/model/Vol.java
+++ src/main/java/com/inti/model/Vol.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
@@ -24,6 +25,7 @@ public class Vol {
     private CompagnieAerienne compagnieaerienne;
     
     @OneToMany
+    @JoinColumn
     private List<Reservation> listeReservation;
     
     private Aeroport aeroport;
